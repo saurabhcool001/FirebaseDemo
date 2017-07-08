64f61efe7120cebf03b1ec5c7a8e34dd80f3b931
Saurabh Parmar <parmarsaurabh87@gmail.com>
2017-07-08 17:39:36 +0530

    Retrive Simple Data to Database

--- app/src/main/java/com/example/saurabh/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/saurabh/firebasedemo/MainActivity.java
@@ -6,62 +6,63 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button mFirebaseBtn;
-    private EditText mValueField;
-    private EditText mEmailId;
+    private DatabaseReference mDatabaseName, mDatabaseEmail;
 
-    private DatabaseReference mDatabase;
+    private TextView mNameField, mEmailField;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mFirebaseBtn = (Button) findViewById(R.id.firebaseBtn);
-        mValueField = (EditText) findViewById(R.id.valueField);
-        mEmailId = (EditText) findViewById(R.id.emailId);
+        mDatabaseName = FirebaseDatabase.getInstance().getReference().child("Name");
+        mDatabaseEmail = FirebaseDatabase.getInstance().getReference().child("Email");
+        mNameField = (TextView) findViewById(R.id.name_Field);
+        mEmailField = (TextView) findViewById(R.id.emailField);
 
-        mDatabase = FirebaseDatabase.getInstance().getReference();
-
-        mFirebaseBtn.setOnClickListener(new View.OnClickListener() {
+        mDatabaseName.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                //Step 1: Create child in root object
-                //Step 2: Assign some value to the child object
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.getValue().toString();
+                mNameField.setText("Name : " + name);
+            }
 
-                String name = mValueField.getText().toString().trim();
-                String email = mEmailId.getText().toString().trim();
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
-                HashMap<String, String> hashMap = new HashMap<String, String>();
-                hashMap.put("Name", name);
-                hashMap.put("Email", email);
-//                mDatabase.push().setValue(hashMap);
-                mDatabase.push().setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        if (task.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "Data Stored", Toast.LENGTH_SHORT).show();
-                        } else {
-                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
-                        }
-                    }
-                });
-
-//                mDatabase.child("Name").setValue(value);
-
-//                mDatabase.push().setValue(value);
             }
         });
+
+        mDatabaseEmail.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String email = dataSnapshot.getValue().toString();
+                mEmailField.setText("Email : " + email);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
+
+    ;
 }
