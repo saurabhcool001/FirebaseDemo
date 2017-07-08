313e4831dce20db63ed51ef1ca0592d084467c2d
Saurabh Parmar <parmarsaurabh87@gmail.com>
2017-07-08 18:36:12 +0530

    Retrive Multiple Data to Database

--- app/src/main/java/com/example/saurabh/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/saurabh/firebasedemo/MainActivity.java
@@ -4,42 +4,75 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
-    private DatabaseReference mDatabaseName, mDatabaseEmail;
+    private DatabaseReference mDatabase;
 
-    private TextView mNameField, mEmailField;
+    private ListView mUserList;
+    private ArrayList<String> mUsername = new ArrayList<>();
+    private ArrayList<String> mKey = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mDatabaseName = FirebaseDatabase.getInstance().getReference().child("Name");
-        mDatabaseEmail = FirebaseDatabase.getInstance().getReference().child("Email");
-        mNameField = (TextView) findViewById(R.id.name_Field);
-        mEmailField = (TextView) findViewById(R.id.emailField);
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+        mUserList = (ListView) findViewById(R.id.userList);
 
-        mDatabaseName.addValueEventListener(new ValueEventListener() {
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mUsername);
+        mUserList.setAdapter(arrayAdapter);
+
+        mDatabase.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String name = dataSnapshot.getValue().toString();
-                mNameField.setText("Name : " + name);
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                String key = dataSnapshot.getKey();
+
+                mUsername.add(value);
+                mKey.add(key);
+
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                String value = dataSnapshot.getValue(String.class);
+                String key = dataSnapshot.getKey();
+
+                int index = mKey.indexOf(key);
+
+                mUsername.set(index, value);
+                arrayAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
             }
 
             @Override
@@ -47,22 +80,5 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-
-        mDatabaseEmail.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                String email = dataSnapshot.getValue().toString();
-                mEmailField.setText("Email : " + email);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-
-    }
-
-    ;
+    };
 }
