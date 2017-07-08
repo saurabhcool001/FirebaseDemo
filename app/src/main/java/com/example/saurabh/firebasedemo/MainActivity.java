81aa7fabd9aaf77b1f3ace6302e38331c036b174
Saurabh Parmar <parmarsaurabh87@gmail.com>
2017-07-08 16:51:37 +0530

    Saving Data to Database

--- app/src/main/java/com/example/saurabh/firebasedemo/MainActivity.java
+++ app/src/main/java/com/example/saurabh/firebasedemo/MainActivity.java
@@ -1,16 +1,25 @@
 package com.example.saurabh.firebasedemo;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button mFirebaseBtn;
+    private EditText mValueField;
+    private EditText mEmailId;
 
     private DatabaseReference mDatabase;
 
@@ -20,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mFirebaseBtn = (Button) findViewById(R.id.firebaseBtn);
+        mValueField = (EditText) findViewById(R.id.valueField);
+        mEmailId = (EditText) findViewById(R.id.emailId);
 
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
@@ -29,7 +40,27 @@ public class MainActivity extends AppCompatActivity {
                 //Step 1: Create child in root object
                 //Step 2: Assign some value to the child object
 
-                mDatabase.child("Name").setValue("Saurabh");
+                String name = mValueField.getText().toString().trim();
+                String email = mEmailId.getText().toString().trim();
+
+                HashMap<String, String> hashMap = new HashMap<String, String>();
+                hashMap.put("Name", name);
+                hashMap.put("Email", email);
+//                mDatabase.push().setValue(hashMap);
+                mDatabase.push().setValue(hashMap).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(MainActivity.this, "Data Stored", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "Error", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+//                mDatabase.child("Name").setValue(value);
+
+//                mDatabase.push().setValue(value);
             }
         });
     }
